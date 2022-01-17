d9243a054bc7b41608fa2707e489c4efff415787
IsaacTorres11 <isaac325z10@gmail.com>
2022-01-16 23:21:26 -0600

    Entregable3

---
+++ src/LocationInfo/LocationInfo.jsx
@@ -0,0 +1,66 @@
+import React,{useEffect, useState} from 'react'
+
+//estilos
+import "./LocationInfo.css"
+
+// Componentes
+import ResidentsList from '../ResidentsList/ResidentsList'
+import SearchBox from '../SearchBox/SearchBox'
+
+//imagenes
+import fondo from '../../src/Imagenes/fondo2.jpg'
+
+const idAleatorio = Math.floor(Math.random() *126) +1;
+console.log('soy el '+ idAleatorio);
+
+const LocationInfo = () => {
+    //State
+    const [terra, setTerra] = useState ({})
+    
+    
+    // useEffect
+    useEffect(() => {
+        const consultaApi = async()=>{
+            const Api = `https://rickandmortyapi.com/api/location/${idAleatorio}`
+            const response = await fetch (Api)
+            const responseJson = await response.json()
+            console.log(responseJson);
+            setTerra(responseJson)
+        }
+        consultaApi ()
+    },[])
+
+    const habitantes = terra.residents
+    
+
+    return (
+        <div className='tierra'>
+            <header className='encabezado'>
+                <div className='imagen'>
+                    <img src={fondo} alt="rickymorty" />
+                </div>
+                <div className='Formulario'>
+                    <SearchBox className='form' setTerra ={setTerra}/>
+                </div>
+                <div className='informacion'>
+                    <h2>Nombre: <span>{terra.dimension}</span> </h2>
+                    <h2>Id: <span>{terra.id}</span> </h2>
+                    <h2>Tipo: <span>{terra.type}</span> </h2>
+                    <h2>Residentes: <span>{terra.residents?.length}</span> </h2>
+                </div>
+            </header>
+            
+            <div className='Personajes'>
+                {
+                    terra.residents?.map(actual=> {
+                        return (
+                            <ResidentsList key={actual} infopersonaje = {actual}/>
+                        ) 
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default LocationInfo
